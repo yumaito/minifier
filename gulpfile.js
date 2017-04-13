0174a7fb583a86b21315f8b73ba98c1b218336b2
ito-yuma <ito-yuma@kayac.com>
2017-04-13 20:03:57 +0900

    ruby-sass

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+
+// JS圧縮
+gulp.task('minify-js', function() {
+    return gulp.src("js/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+// sassコンパイル
+gulp.task('sass', function() {
+    return sass('scss/*.scss', {
+        style: 'compressed',
+        sourcemap: false,
+        noCache: true
+    }).on('error', sass.logError)
+    .pipe(gulp.dest('dist/css/'));
+});
+
+// 以下を追加
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+});
