4d10c6af595b0d6943008f20a54d6328d85bd843
yumaito-mac <yumaito0@gmail.com>
2017-04-13 20:29:26 +0900

    minimist

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-ruby-sass');
+var minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2));
+console.log(args)
 
 // JS圧縮
 gulp.task('minify-js', function() {
